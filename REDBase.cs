a1a4fb5daf8d5335fad7b8951ac84a6c6fb04fad
REC&D <tinhbuonbg@gmail.com>
2021-06-12 10:05:42 +0700

    Add base class

---
+++ REDBase.cs
@@ -0,0 +1,15 @@
+﻿using de4dot.code;
+using dnlib.DotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RED_Template
+{
+    public abstract class REDBase
+    {
+        protected abstract void DO(ModuleDefMD module);
+    }
+}
